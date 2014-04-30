db20c7aa5a717cb26af4e31fbcb12ad7ec2de81c
Joakin <joaquin@chimeces.com>
2014-04-30 13:02:49 +0200

    Cleanup

--- lt-plugins/src/lt_plugins/core.cljs
+++ lt-plugins/src/lt_plugins/core.cljs
@@ -9,21 +9,6 @@
 
 (def app-dom (.querySelector js/document "#app"))
 
-
-;; {:added 1389153835495,
-;;  :info {:dependencies {},
-;;         :name Clojure,
-;;         :version 0.0.10,
-;;         :author Kodowa,
-;;         :source https://github.com/LightTable/Clojure,
-;;         :desc Clojure integration for Light Table,
-;;         :behaviors clojure.behaviors},
-;;  :installs 10290,
-;;  :repo Clojure,
-;;  :updated 1397752711508,
-;;  :url https://github.com/LightTable/Clojure,
-;;  :user LightTable}
-
 (defn init []
   (let [data {:plugins (ajax/get-plugins)}]
     (reagent/render-component [app data]
